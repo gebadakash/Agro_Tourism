2d8a4d5c7d3352b0fb39f904c4977740ecbeb139
akashgebad <akashgebad45@gmail.com>
2024-05-08 14:58:35 +0530

    url changes.

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,11 @@ app.use(cors({
 }));
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 pool.on('error', (err, client) => {
